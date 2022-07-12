073e697e85eedc04cee57ddf6f0a5dae5f74c847
Dixon Pedraza <pedrazadixon@gmail.com>
2022-07-12 16:57:18 -0500

    cambios

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import webview
+import re
+import shutil
 
 # shorthands
 nw = subprocess.CREATE_NO_WINDOW
@@ -8,42 +10,87 @@ nw = subprocess.CREATE_NO_WINDOW
 
 # paths
 work_dir = os.getcwd()
-apache_dir = os.path.join(work_dir, 'bin', 'Apache24')
+apache_dir = os.path.join(work_dir, 'bin', 'apache')
 php_dir = os.path.join(work_dir, 'bin', 'php')
+php_ext_dir = os.path.join(php_dir, 'ext')
 mariadb_dir = os.path.join(work_dir, 'bin', 'mariadb')
 www_dir = os.path.join(work_dir, 'www')
 
-# create folder www 
+
+# create folder www if required
 if os.path.exists(www_dir) == False:
     os.mkdir(www_dir)
 
-# make httpd configd
-httpd_ori = open(os.path.join(apache_dir, 'conf', 'httpd.ori'), "rt")
-data = httpd_ori.read()
-data = data.replace('[PHPIniDir]', php_dir)
-data = data.replace('[php7_module]', os.path.join(
-    php_dir, 'php7apache2_4.dll'))
-data = data.replace('[SRVROOT]', apache_dir)
-data = data.replace('[DocumentRoot]', www_dir)
-httpd_ori.close()
+
+# make apache config
+httpd_conf = open(os.path.join(apache_dir, 'conf', 'httpd.conf'), "rt")
+data = httpd_conf.read()
+httpd_conf.close()
+
+httpd_SRVROOT = 'Define SRVROOT "{}"'.format(apache_dir.replace('\\', '\\\\'))
+httpd_Listen = 'Listen 127.0.0.1:8001'
+httpd_DocumentRoot = 'DocumentRoot "{}"'.format(www_dir.replace('\\', '\\\\'))
+httpd_Directory = '<Directory "{}">'.format(www_dir.replace('\\', '\\\\'))
+httpd_PHPIniDir = 'PHPIniDir "{}"'.format(php_dir.replace('\\', '\\\\'))
+httpd_php_mod_directive = 'php7_module'
+httpd_php_mod_path = os.path.join(php_dir, 'php7apache2_4.dll')
+httpd_php_mod_string = 'LoadModule {} "{}"'.format(httpd_php_mod_directive, httpd_php_mod_path.replace('\\', '\\\\'))
+
+data = re.sub(r'(^Define SRVROOT .*)', httpd_SRVROOT, data, flags=re.M)
+data = re.sub(r'(^Listen .*)', httpd_Listen, data, flags=re.M)
+data = re.sub(r'(^DocumentRoot .*)', httpd_DocumentRoot, data, flags=re.M)
+data = re.sub(r'(^<Directory .*(htdocs|www).*>$)', httpd_Directory, data, flags=re.M)
+data = re.sub(r'(AllowOverride None)', 'AllowOverride All', data, flags=re.M)
+data = re.sub(r'(DirectoryIndex index.html)', 'DirectoryIndex index.php index.html', data, flags=re.M)
+
+if "PHPIniDir " in data:
+    data = re.sub(r'(^PHPIniDir .*)', httpd_PHPIniDir, data, flags=re.M)
+else:
+    data = data + '\n' + httpd_PHPIniDir.replace('\\\\', '\\')
+
+if "LoadModule php" in data:
+    data = re.sub(r'(^LoadModule php.*)', httpd_php_mod_string, data, flags=re.M)
+else:
+    data = data + '\n' + httpd_php_mod_string.replace('\\\\', '\\')
+
+if not "AddType application/x-httpd-php .php" in data:
+    data = data + '\nAddType application/x-httpd-php .php'
 
 httpd_conf = open(os.path.join(apache_dir, 'conf', 'httpd.conf'), "wt")
 httpd_conf.write(data)
 httpd_conf.close()
 
 
+# make php config
+if os.path.exists(os.path.join(php_dir, 'php.ini')) == False:
+    shutil.copyfile(os.path.join(php_dir, 'php.ini-development'), os.path.join(php_dir, 'php.ini'))
+
+php_ini = open(os.path.join(php_dir, 'php.ini'), "rt")
+data = php_ini.read()
+php_ini.close()
+
+data = re.sub(r'(^;extension_dir = "ext".*)',
+              'extension_dir = "{}"'.format(php_ext_dir.replace('\\', '\\\\')), data, flags=re.M)
+data = re.sub(r'(^;extension=intl.*)', 'extension=intl', data, flags=re.M)
+data = re.sub(r'(^;extension=mbstring.*)', 'extension=mbstring', data, flags=re.M)
+data = re.sub(r'(^;extension=mysqli.*)', 'extension=mysqli', data, flags=re.M)
+data = re.sub(r'(^;extension=openssl.*)', 'extension=openssl', data, flags=re.M)
+data = re.sub(r'(^;extension=pdo_mysql.*)', 'extension=pdo_mysql', data, flags=re.M)
+
+php_ini = open(os.path.join(php_dir, 'php.ini'), "wt")
+php_ini.write(data)
+php_ini.close()
+
+
 # initialize mysql data if required
 if os.path.exists(os.path.join(mariadb_dir, 'data')) == False:
-    subprocess.Popen(
-        os.path.join(mariadb_dir, 'bin', 'mysql_install_db.exe'), creationflags=nw).wait()
+    subprocess.Popen(os.path.join(mariadb_dir, 'bin', 'mysql_install_db.exe'), creationflags=nw).wait()
 
 
 # start_servers
-mysql_server = subprocess.Popen(
-    [os.path.join(mariadb_dir, 'bin', 'mariadbd.exe'), '--port=8006'], creationflags=nw)
+mysql_server = subprocess.Popen([os.path.join(mariadb_dir, 'bin', 'mariadbd.exe'), '--port=8006'], creationflags=nw)
 
-httpd_server = subprocess.Popen(
-    [os.path.join(apache_dir, 'bin', 'httpd.exe')], creationflags=nw)
+httpd_server = subprocess.Popen([os.path.join(apache_dir, 'bin', 'httpd.exe')], creationflags=nw)
 
 
 # start webview
